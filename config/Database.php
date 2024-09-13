06c83fcac069d555a59f5088f8db85493f237cbf
uzziahlukeka <uzziahlukeka@gmail.com>
2024-09-13 17:46:30 +0200

    adding docker

--- config/Database.php
+++ config/Database.php
@@ -4,11 +4,11 @@ use PDO;
 use PDOException;
 
 class Database{
-    private $host = '127.0.0.1';
+    private $host = 'database';
     private $db_name = 'log2';
-    private $db_port = 3306; // Assuming default port for MySQL
-    private $username = 'theend';
-    private $password = 'Riw[GBo(sKS]hgHM';
+    //private $db_port = 3306; // Assuming default port for MySQL
+    private $username = 'uzh';
+    private $password = 'test';
     private $conn;
 
     // DB connect 
@@ -17,7 +17,7 @@ class Database{
 
         try{
             // Include port number in the DSN if it's not the default port
-            $this->conn = new PDO('mysql:host='.$this->host.';port='.$this->db_port.';dbname='.$this->db_name, $this->username, $this->password);
+            $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name, $this->username, $this->password);
             // Set attributes for showing errors  
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e){
--- getPath.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "Current directory is: " . __DIR__;
